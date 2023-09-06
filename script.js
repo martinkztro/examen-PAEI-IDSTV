b7e674899207920b580bb0c10e6a55b20b0a4a98
Martin Castro <devmcastro02@gmail.com>
2023-09-05 23:36:43 -0700

    chore: agregar recursos de imagenes e imagen de fondo.

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+//player
+const rocketOff = new Image();
+rocketOff.src = 'rocket-off.png';
+const rocketOn = new Image();
+rocketOn.src = 'rocket-on.png';
+const rocketXplode = new Image();
+rocketXplode.src = 'rocket-xplode.png';
+
+//enemies
+const asteroid = new Image();
+asteroid.src = 'asteroid.png';
+const blackHole = new Image();
+blackHole.src = 'black-hole.png';
+
+//coins
+const stars = new Image();
+stars.src = 'stars.png';
+
+//goal
+const moon = new Image();
+moon.src = 'moon.png';
+
+

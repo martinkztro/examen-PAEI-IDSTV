a83612c9148ef023490ccb7f1576f8399c146838
Martin Castro <devmcastro02@gmail.com>
2023-09-07 19:58:23 -0700

    style: agregar muros del mapa, un enemigo y 2 personajes extras

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-//player
-const rocketOff = new Image();
-rocketOff.src = 'rocket-off.png';
-const rocketOn = new Image();
-rocketOn.src = 'rocket-on.png';
-const rocketXplode = new Image();
-rocketXplode.src = 'rocket-xplode.png';
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
 
+canvas.width = map[0].length * 40;
+canvas.height = map.length * 40;   
+
+//pintar mapa
+function drawMap() {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            if (map[i][j] === 1) {
+                ctx.strokeStyle = 'white';
+                ctx.strokeRect(j * 40, i * 40, 40, 40);
+            } else if (map[i][j] === 2) {
+
+            } else if (map[i][j] === 0) {
+
+            }
+        }
+    }
+}
+
+drawMap();
+
+// DECLARACION DE CLASES
+//player
+class Player {
+    counsructor() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 50;
+        this.height = 50;
+        this.speed = 5;
+        this.img = rocketOff;
+        this.lives = 3;
+    }
+}
+const player = new Player(5, 10, 50, 50, 5, 3);
+
+
+// DECLARACION DE PERSONAJES
 //enemies
 const asteroid = new Image();
-asteroid.src = 'asteroid.png';
+asteroid.src = 'images/asteroid.png';
+
+
 const blackHole = new Image();
-blackHole.src = 'black-hole.png';
+blackHole.src = 'images/black-hole.png';
+blackHole.onload = () =>
+ctx.drawImage(blackHole, canvas.width/2 - 160, canvas.height/2 - 90, 320, 180);
+
+//portals
+const wormHoleTop = new Image();
+wormHoleTop.src = 'images/worm-hole.png';
+wormHoleTop.onload = () => ctx.drawImage(wormHoleTop, 475, 50, 170, 130);
+
+const wormHoleBottom = new Image();
+wormHoleBottom.src = 'images/worm-hole.png';
+wormHoleBottom.onload = () => ctx.drawImage(wormHoleBottom, 475, 415, 170, 130);
+
 
 //coins
-const stars = new Image();
-stars.src = 'stars.png';
-
-//goal
-const moon = new Image();
-moon.src = 'moon.png';
-
-
+const stars = new Image(); //Esto no será imagen, será una figura de canvas
